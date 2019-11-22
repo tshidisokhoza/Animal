ead536db11a7f622fd60cb16c85d0b92c14e5dd9
tshidisokhoza <tshidiso.khoza@umuzi.org>
2019-11-22 15:42:54 +0200

    Animal

---
+++ Animal.java
@@ -0,0 +1,15 @@
+class Animal {
+    private String name;
+   
+    void sound() {
+        System.out.println("sound...");
+    }
+    void eat() {
+        System.out.println(name+" eats.");
+    }
+
+ void setName(String name) {
+        this.name = name;
+    }
+
+}
